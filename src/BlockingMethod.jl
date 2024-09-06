3709ba35488f3d7698b553df40b5a8ac22f5d400
David <davidabraham@ucla.edu>
2024-09-06 12:35:23 -0700

    updated documentation and added Typing to make computing more efficient

--- src/BlockingMethod.jl
+++ src/BlockingMethod.jl
@@ -5,49 +5,55 @@ module BlockingMethod
 export estimate
 
 """
-    estimate(x::AbstractArray)
+    estimate(x::Vector{Float64})::NTuple{2, Float64}
 
 Performs time series analysis on the `x` column of data outputting
-the mean and standard error.
+the mean and standard error (variance).
 
-Reference Flyvbjerb and Petersen '89 for more info.
+# Flyvbjerb and Petersen 1989 - Abstract
+We describe how the true statistical error on an average of correlated data
+can be obtained with ease and efficiency by renormalization group method [...]
+Reference article https://doi.org/10.1063/1.457480 for more info.
 """
-function estimate(x::AbstractArray)
+function estimate(x::Vector{Float64})::NTuple{2, Float64}
 
     n = length(x)
+    fn = Float64(n)
 
-    xm = sum(x) / n
-    x2m = sum(y -> y^2, x) / n
-    σ = 0
+    xm = sum(x) / fn
+    x2m = sum(y -> y^2, x) / fn
+    σ = 0.0
 
     # if too small exit out
     if (n < 2) @goto fin end
 
     # number of bins
-    nrbin = log(n) / log(2) - 1
+    nrbin = log(fn) / log(2) - 1.
 
-    ci = (x2m - xm^2) / (n - 1)
-    faci = 1 / sqrt(2*(n - 1))
+    c_max = (x2m - xm^2) / (fn - 1.)
+    fac = 1 / sqrt(2*fn - 2.)
 
-    dc = faci * ci
+    dc = fac * c_max
 
-    c_max = ci
-    c_old = 0
+    c_old = 0.0
 
     # calculating sigma
     for i in 1:nrbin
 
-        c = 0
+        c = 0.0
         n = floor(Int64, n / 2)
+        fn = Float64(n)
 
         for j in 1:n
-            if n != 1
-                x[j] = (x[2*j] + x[2*j-1]) / 2
-                c += (x[j] - xm)^2 / (n * (n - 1))
+            if(n == 1)
+                continue
+            else
+                fac = 1 / sqrt(2*fn - 2.)
+                x[j] = (x[2*j] + x[2*j-1]) / 2.
+                c += (x[j] - xm)^2 / (fn^2 - fn)
             end
         end
 
-        fac = 1 / sqrt(2*(n - 1))
         dc = fac*sqrt(c)
         diff = sqrt(c) - sqrt(c_old)
 
@@ -61,7 +67,6 @@ function estimate(x::AbstractArray)
 
     @label fin
     return (xm, σ)
-
 end
 
 end # module
